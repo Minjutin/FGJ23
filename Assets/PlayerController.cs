313b30b24524c5e5927f929d842cfb8863041cc9
Milja <91054318+Minjutin@users.noreply.github.com>
2023-02-04 22:02:57 +0200

    Edited character move

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,18 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 1.0f, Spin = 1.0f, verticalInput, horizontalInput;
+    public float moveSpeed = 100.0f, Spin = 1.0f, verticalInput, horizontalInput;
+
+    public Transform orientation;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
-    void Update()
-    {       
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.left * movementSpeed * verticalInput * Time.deltaTime);
-        transform.Rotate(Vector3.up * Spin * horizontalInput * Time.deltaTime);
+    private void Update()
+    {
+        MyInput();
+        SpeedControl();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+
+        rb.AddForce(orientation.forward*verticalInput*moveSpeed, ForceMode.Acceleration);
+        transform.Rotate(Vector3.up * Spin * horizontalInput*Time.deltaTime);
+
+        if (verticalInput ==0)
+        {
+            rb.velocity = new Vector3(0,0,0);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
